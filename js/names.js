f1af5caeb5e01340366aa3e15eed3df282c9ee02
Dmtro_Tr <Di4gamegem@gmail.com>
2024-09-28 13:58:00 +0300

    hv6

--- js/names.js
+++
@@ -1,9 +0,0 @@
-const { books } = require('./task-1');
-
-export const names = books
-  .map(autors => autors.author)
-  .filter(
-    (ratingAll, MIN_BOOK_RATING, array) =>
-      array.rating(ratingAll) > MIN_BOOK_RATING
-  )
-  .toSorted((a, b) => a.localeCompare(b));
